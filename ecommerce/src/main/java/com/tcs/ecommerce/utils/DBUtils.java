f620545aa0f3fcfede4592b1de52e193b57c08ca
sudheer nimmagadda <snimmagadda2@student.gsu.edu>
2020-11-11 13:39:44 -0600

    ecommerce project created and updated

---
+++ ecommerce/src/main/java/com/tcs/ecommerce/utils/DBUtils.java
@@ -0,0 +1,37 @@
+package com.tcs.ecommerce.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+	
+	public static Connection getConnection() {
+		
+		Connection connection=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/tcs","root","y13cs958");
+			
+			return connection;
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return connection;
+	}
+	
+	public static void closeConnection(Connection connection) {
+		
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
